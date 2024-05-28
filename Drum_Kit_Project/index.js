d9dd678b4d56124cab8d4e884c1f722dcae86dde
azar <azar24feb@gmail.com>
2024-05-29 01:11:22 +0530

    123

--- Drum_Kit_Project/index.js
+++ Drum_Kit_Project/index.js
@@ -3,31 +3,49 @@
         var audio = new Audio("./sounds/tom-1.mp3")
         audio.play()
     }));*/
-document.querySelector(".w").addEventListener("click", () => {
-    var audio = new Audio("./sounds/tom-1.mp3")
-    audio.play()
-});
-document.querySelector(".a").addEventListener("click", () => {
-    var audio = new Audio("./sounds/tom-2.mp3")
-    audio.play()
-});
-document.querySelector(".s").addEventListener("click", () => {
-    var audio = new Audio("./sounds/tom-3.mp3")
-    audio.play()
-});
-document.querySelector(".d").addEventListener("click", () => {
-    var audio = new Audio("./sounds/tom-4.mp3")
-    audio.play()
-});
-document.querySelector(".j").addEventListener("click", () => {
-    var audio = new Audio("./sounds/snare.mp3")
-    audio.play()
-});
-document.querySelector(".k").addEventListener("click", () => {
-    var audio = new Audio("./sounds/crash.mp3")
-    audio.play()
-});
-document.querySelector(".l").addEventListener("click", () => {
-    var audio = new Audio("./sounds/kick-bass.mp3")
-    audio.play()
-});
+document.querySelectorAll(".drum")
+    .forEach(x => {
+        x.addEventListener("click", function (params) {
+            this.style.color = 'white';
+            play(x.classList[0]);
+        })
+    })
+
+document.addEventListener('keydown', (e) => {
+     play(e.key);
+})
+
+function play(key){
+    switch (key.toLowerCase()) {
+        case 'w':
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('./sounds/kick.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
